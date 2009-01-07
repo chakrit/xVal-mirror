72f29a593de7c9e1584c45d0e00f84294f9ef6a3
SND\SteveSanderson_cp <SND\SteveSanderson_cp@8ead0314-7f71-49e1-95c8-3147638646d4>
2009-01-07 11:45:24 +0000

    Formatting rule data as custom XHTML element set
    git-svn-id: https://xval.svn.codeplex.com/svn@2235 8ead0314-7f71-49e1-95c8-3147638646d4

--- xVal.Tests/HtmlHelpers/DefaultValidationConfigFormatterTests.cs
+++ xVal.Tests/HtmlHelpers/DefaultValidationConfigFormatterTests.cs
@@ -1,5 +1,10 @@
+using System.Collections.Generic;
+using System.Web.Routing;
 using Xunit;
 using xVal.Html;
+using xVal.RuleProviders;
+using xVal.Rules;
+using System.Linq;
 
 namespace xVal.Tests.HtmlHelpers
 {
@@ -12,9 +17,93 @@ namespace xVal.Tests.HtmlHelpers
             var formatter = new DefaultValidationConfigFormatter();
 
             // Act
-            //var result = formatter.FormatRules();
+            var result = formatter.FormatRules(RuleSet.Empty, "myname");
 
             // Assert
+            Assert.Equal("<ruleset name=\"myname\" />", result);
+        }
+
+        [Fact]
+        public void Single_Rule()
+        {
+            // Arrange
+            var formatter = new DefaultValidationConfigFormatter();
+            var rules = MakeTestRuleSet(new Dictionary<string, IDictionary<string, object>> {
+                { 
+                    "myprop", new Dictionary<string, object> {
+                        { "somerule", new { param1 = "param1value", param2 = "param2value" } }    
+                    } 
+                }
+            });
+
+            // Act
+            var result = formatter.FormatRules(rules, "myname");
+
+            // Assert
+            Assert.Equal(@"<ruleset name=""myname"">
+    <somerule forfield=""myprop"" param1=""param1value"" param2=""param2value"" />
+</ruleset>", result);
+        }
+
+        [Fact]
+        public void Multiple_Rules()
+        {
+            // Arrange
+            var formatter = new DefaultValidationConfigFormatter();
+            var rules = MakeTestRuleSet(new Dictionary<string, IDictionary<string, object>> {
+                { 
+                    "screenplay", new Dictionary<string, object> {
+                        { "copyright", new { author = "Wm. Shakespeare", year = "1668" } },
+                        { "description", new { language = "Welsh", grammar = "perfect", length = "long" } }    
+                    }
+                },
+                {
+                    "petname", new Dictionary<string, object> {
+                        { "required", new { } },
+                        { "DataType", new { @type = "EmailAddress", domainSuffix = ".co.uk" } },
+                        { "LengthConstraint", new { max = "150" } }
+                    }
+                }
+            });
+
+            // Act
+            var result = formatter.FormatRules(rules, "somerules");
+
+            // Assert
+            // Notice that the attributes are re-ordered (into alphabetical order)
+            // This test code is a bit flaky because the attributes could be ordered differently in future
+            // Not sure if there's a better way to test this (maybe will parse the output as XML then inspect independently of order)
+            Assert.Equal(@"<ruleset name=""somerules"">
+    <copyright author=""Wm. Shakespeare"" forfield=""screenplay"" year=""1668"" />
+    <description forfield=""screenplay"" grammar=""perfect"" language=""Welsh"" length=""long"" />
+    <required forfield=""petname"" />
+    <DataType domainSuffix="".co.uk"" forfield=""petname"" type=""EmailAddress"" />
+    <LengthConstraint forfield=""petname"" max=""150"" />
+</ruleset>", result);
+        }
+
+        // Utility function to give a quick syntax for instantiating a complete RuleSet
+        private RuleSet MakeTestRuleSet(IDictionary<string, IDictionary<string, object>> data)
+        {
+            var rules = from propName in data.Keys
+                        from ruleName in data[propName].Keys
+                        select new { propName, rule = (RuleBase) new TestRule(ruleName, data[propName][ruleName]) };
+            return new RuleSet(rules.ToLookup(x => x.propName, x => x.rule));
+        }
+
+        private class TestRule : RuleBase
+        {
+            public IDictionary<string, string> Parameters { get; set; }
+
+            public TestRule(string ruleName, object parameters) : base(ruleName)
+            {
+                Parameters = new RouteValueDictionary(parameters).ToDictionary(x => x.Key, x => x.Value.ToString());
+            }
+
+            public override IDictionary<string, string> ListParameters()
+            {
+                return Parameters;
+            }
         }
     }
 }
--- xVal/Html/DefaultValidationConfigFormatter.cs
+++ xVal/Html/DefaultValidationConfigFormatter.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Linq;
+using System.Text;
+using System.Web.Mvc;
 using xVal.RuleProviders;
 using xVal.Rules;
 
@@ -6,9 +9,38 @@ namespace xVal.Html
 {
     public class DefaultValidationConfigFormatter : IValidationConfigFormatter
     {
+        private const string TagName = "ruleset";
+        private const string NewLine = "\r\n";
+        private const string Indent = "    ";
+
         public string FormatRules(RuleSet rules, string rulesetName)
         {
-            return null;
+            var tb = new TagBuilder(TagName);
+            tb.MergeAttribute("name", rulesetName);
+
+            var rulesBuilder = new StringBuilder();
+            var allRules = from key in rules.Keys
+                           from rule in rules[key]
+                           select new { key, rule };
+
+            if (allRules.Any())
+                rulesBuilder.Append(NewLine);
+            foreach (var item in allRules) {
+                rulesBuilder.Append(Indent);
+                rulesBuilder.Append(FormatSingleRule(item.rule, item.key));
+                rulesBuilder.Append(NewLine);
+            }
+
+            tb.InnerHtml = rulesBuilder.ToString();
+            return tb.ToString(allRules.Any() ? TagRenderMode.Normal : TagRenderMode.SelfClosing);
+        }
+
+        private static string FormatSingleRule(RuleBase rule, string forField)
+        {
+            var tb = new TagBuilder(rule.RuleName);
+            tb.MergeAttributes(rule.ListParameters());
+            tb.MergeAttribute("forfield", forField);
+            return tb.ToString(TagRenderMode.SelfClosing);
         }
     }
 }

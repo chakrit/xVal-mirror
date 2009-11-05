0dff56754ef1972527de750af583dd93379b0c2b
SND\SteveSanderson_cp <SND\SteveSanderson_cp@8ead0314-7f71-49e1-95c8-3147638646d4>
2009-11-05 16:20:41 +0000

    Added Romanian (ro-RO) translations thanks to Andrei Rinea
    git-svn-id: https://xval.svn.codeplex.com/svn@31451 8ead0314-7f71-49e1-95c8-3147638646d4

---
+++ xVal.ClientSidePlugins/Messages/xVal.Messages.ro-RO.js
@@ -0,0 +1,35 @@
+﻿/* If you want default messages in English, you don't need to reference this file, because the messages are 
+ * in English by default anyway. This file exists only for reference during translation.
+ *
+ * To get default messages in any other language (or a different dialect of English):
+ *  (1) Make a copy of this file and call it anything else, for example: xVal.Messages.fr-FR.js
+ *  (2) Edit your copy to specify messages in your chosen language
+ *  (3) Reference your file in your project using a <script> tag.
+ * Simply referencing your custom file will override the default messages with your messages.
+*/
+var xVal = xVal || {};
+xVal.Messages = {
+    "Required" : "Această valoare trebuie introdusă.",
+    "DataType_EmailAddress" : "Vă rugăm introduceţi o adresă de e-mail validă.",
+    "DataType_Integer" : "Vă rugăm introduceţi un număr întreg.",
+    "DataType_Decimal" : "Vă rugăm introduceţi un număr.",
+    "DataType_Date" : "Vă rugăm introduceţi o dată validă.",
+    "DataType_DateTime" : "Vă rugăm introduceţi o dată şi o oră.",
+    "DataType_Currency" : "Vă rugăm introduceţi o sumă de bani.",
+    "DataType_CreditCardLuhn" : "Vă rugăm introduceţi un număr valid de card de credit.",
+    "Regex" : "Valoarea nu este validă.",
+    "Range_Numeric_Min" : "Vă rugăm introduceţi o valoare de măcar {0}.",
+    "Range_Numeric_Max" : "Vă rugăm introduceţi o valoare mai mică sau egală cu {0}.",
+    "Range_Numeric_MinMax" : "Vă rugăm introduceţi o valoare între {0} şi {1}.",
+    "Range_String_Min" : "Vă rugăm introduceţi o valoare nealfabetică înainte de '{0}'.",
+    "Range_String_Max" : "Vă rugăm introduceţi o valoare nealfabetică după '{0}'.",
+    "Range_String_MinMax" : "Vă rugăm introduceţi o valoare alfabetică între '{0}' şi '{1}'.",
+    "Range_DateTime_Min" : "Vă rugăm introduceţi o dată nu mai devreme de {0}.",
+    "Range_DateTime_Max": "Vă rugăm introduceţi o dată nu după {0}.",
+    "Range_DateTime_MinMax": "Vă rugăm introduceţi o dată între {0} and {1}.",
+    "StringLength_Min": "Vă rugăm introduceţi măcar {0} caractere.",
+    "StringLength_Max": "Vă rugăm introduceţi nu mai mult de {0} caractere.",
+    "StringLength_MinMax": "Vă rugăm introduceţi între {0} şi {1} caractere.",
+    "Comparison_Equals" : "Această valoare trebuie să fie la fel cu {0}.",
+    "Comparison_DoesNotEqual" : "Această valoare trebuie să fie diferită de {0}."
+};
